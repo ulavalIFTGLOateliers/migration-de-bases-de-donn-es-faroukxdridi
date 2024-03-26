cca3ea0e467790eae4b3a1dbd7ba4b823a0e3390
faroukxdridi <farouk.dridi.1@ulaval.ca>
2024-03-26 18:33:47 -0400

    aloo

--- db_scripts/migrate_1.sql
+++ db_scripts/migrate_1.sql
@@ -1,13 +1,18 @@
 RENAME TABLE singer TO musician;
 
-CREATE TABLE band (
-    bandName VARCHAR(50),
-    creation YEAR,
-    genre VARCHAR(50)
-);
+ALTER TABLE musician ADD COLUMN role VARCHAR(50);
+ALTER TABLE musician ADD COLUMN bandName VARCHAR(50);
 
-ALTER TABLE band
-ADD PRIMARY KEY (bandName);
+
+ALTER TABLE musician CHANGE singerName musicianName VARCHAR(50);
+
+
+CREATE TABLE IF NOT EXISTS band (
+  bandName VARCHAR(50),
+  creation YEAR,
+  genre VARCHAR(50),
+  PRIMARY KEY (bandName)
+);
 
 
 INSERT INTO band (bandName, creation, genre) VALUES
@@ -15,21 +20,10 @@ INSERT INTO band (bandName, creation, genre) VALUES
 ('Luna', 2009, 'classical'),
 ('Mysterio', 2019, 'pop');
 
-ALTER TABLE album
-    CHANGE singerName musicianName VARCHAR(50);
 
-ALTER TABLE album
-    ADD CONSTRAINT fk_musician
-    FOREIGN KEY (musicianName) REFERENCES musician(musicianName);
 
-ALTER TABLE album
-    ADD CONSTRAINT fk_label
-    FOREIGN KEY (labelName) REFERENCES label(labelName);
 
-ALTER TABLE musician
-    ADD COLUMN role VARCHAR(50),
-    ADD COLUMN bandName VARCHAR(50);
-
-ALTER TABLE musician
-    ADD CONSTRAINT fk_band
-    FOREIGN KEY (bandName) REFERENCES band(bandName);
+UPDATE musician SET role = 'vocals', bandName = 'Crazy Duo' WHERE firstName = 'Darcy' AND lastName = 'Boles';
+UPDATE musician SET role = 'guitar', bandName = 'Mysterio' WHERE firstName = 'Jessie' AND lastName = 'Chancey';
+UPDATE musician SET role = 'percussion', bandName = 'Crazy Duo' WHERE firstName = 'Sarah' AND lastName = 'Derrick';
+UPDATE musician SET role = 'piano', bandName = 'Luna' WHERE firstName = 'Emily' AND lastName = 'Seibold';
--- db_scripts/rollback_1.sql
+++ db_scripts/rollback_1.sql
@@ -1,10 +1,13 @@
-ALTER TABLE musician DROP FOREIGN KEY fk_band;
-ALTER TABLE musician DROP COLUMN role, DROP COLUMN bandName;
+ALTER TABLE album DROP FOREIGN KEY FK_A_musicianName;
 
-ALTER TABLE album DROP FOREIGN KEY fk_musician;
-ALTER TABLE album DROP FOREIGN KEY fk_label;
-ALTER TABLE album CHANGE musicianName singerName VARCHAR(50);
+ALTER TABLE musician DROP FOREIGN KEY fk_musician;
+
+ALTER TABLE musician CHANGE musicianName singerName VARCHAR(50);
+
+ALTER TABLE musician DROP COLUMN role;
+ALTER TABLE musician DROP COLUMN bandName;
+
+RENAME TABLE musician TO singer;
 
 DROP TABLE IF EXISTS band;
 
-RENAME TABLE musician TO singer;
